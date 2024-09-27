621f02a120017b6ff6c97216ee1153fa3c2c1a68
Nguyen Trung Nghia <assassincreed2k1@gmail.com>
2024-09-27 09:53:33 +0700

    Create Respo

---
+++ src/main/java/AppLauncher.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+import javax.swing.SwingUtilities;
+
+public class AppLauncher {
+    public static void main(String args[]) {
+        // Launch App code
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+}
